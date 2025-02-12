b0fcdb1d8b354eb7e9ca34977fbaca069ccdbefd
Hugo bessa <hugo@vinta.com.br>
2025-02-12 03:39:11 -0300

    Updates to vintasend 0.1.1

--- nodemailer-notification-adapter.ts
+++ nodemailer-notification-adapter.ts
@@ -7,12 +7,23 @@ import type { Notification } from 'vintasend/src/types/notification';
 import type { JsonObject } from 'vintasend/src/types/json-values';
 import type { NotificationType } from 'vintasend/src/types/notification-type';
 import type { ContextGenerator } from 'vintasend/src/services/notification-context-registry';
+import type { Identifier } from 'vintasend/src/types/identifier';
 
 export class NodemailerNotificationAdapter<
   TemplateRenderer extends BaseEmailTemplateRenderer<AvailableContexts>,
   Backend extends BaseNotificationBackend<AvailableContexts>,
-  AvailableContexts extends Record<string, ContextGenerator>
-> implements BaseNotificationAdapter<TemplateRenderer, Backend, AvailableContexts> {
+  AvailableContexts extends Record<string, ContextGenerator>,
+  NotificationIdType extends Identifier = Identifier,
+  UserIdType extends Identifier = Identifier,
+> implements
+    BaseNotificationAdapter<
+      TemplateRenderer,
+      Backend,
+      AvailableContexts,
+      NotificationIdType,
+      UserIdType
+    >
+{
   private transporter: nodemailer.Transporter;
   public readonly notificationType: NotificationType = 'EMAIL';
   public readonly key = 'nodemailer';
@@ -21,19 +32,22 @@ export class NodemailerNotificationAdapter<
     public templateRenderer: TemplateRenderer,
     public backend: Backend,
     public readonly enqueueNotifications: boolean,
-    transportOptions: nodemailer.SendMailOptions
+    transportOptions: nodemailer.SendMailOptions,
   ) {
     this.transporter = nodemailer.createTransport(transportOptions);
   }
 
-  async send(notification: Notification<AvailableContexts>, context: JsonObject): Promise<void> {
+  async send(
+    notification: Notification<AvailableContexts, NotificationIdType, UserIdType>,
+    context: JsonObject,
+  ): Promise<void> {
     const template = await this.templateRenderer.render(notification, context);
 
     if (!notification.id) {
       throw new Error('Notification ID is required');
     }
 
-    const userEmail = await this.backend.getUserEmailFromNotification(notification.id)
+    const userEmail = await this.backend.getUserEmailFromNotification(notification.id);
 
     if (!userEmail) {
       throw new Error('User email not found');
